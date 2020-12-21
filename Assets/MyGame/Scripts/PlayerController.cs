702578f80c6d80b97ef5755e133976248385dc5a
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2020-12-21 13:14:44 +0100

    controller working move horizontal

---
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+
+    private float moveHorizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        moveHorizontal = moveHorizontal * Time.deltaTime;
+
+        Debug.Log("x axis" + player.GetComponent<Transform>().position.x);
+
+        player.GetComponent<Transform>().position = player.GetComponent<Transform>().position + new Vector3(moveHorizontal, 0, 0);
+    }
+}
