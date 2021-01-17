981f0c8407b234d15181271fd54401210c605796
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2021-01-17 18:15:50 +0100

    add Rigidbody2D & Collider2D

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    int countCollision = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.name == "Present")
+        {
+            Debug.Log("in if in Present + 1");
+            countCollision++;
+        }
+
+        Debug.Log("count collisions" + countCollision);
+        Debug.Log("IN PLAYER: Collide with " + collision.name);
+    }
+}
